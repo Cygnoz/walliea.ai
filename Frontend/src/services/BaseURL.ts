d4f757510d28136c1651befefb156f2200521d1d
Akhil Sajeevan <akhilsajeevan@cygnonex.com>
2025-02-13 15:14:17 +0530

    base url changed

--- Frontend/src/services/BaseURL.ts
+++ Frontend/src/services/BaseURL.ts
@@ -2,5 +2,8 @@
 
 //const BASE_URL: string = 'http://13.232.79.148:5001';
 
-const BASE_URL: string = 'https://www.walliea.ai:5001';
+// const BASE_URL: string = 'https://www.walliea.ai:5001';
+
+const BASE_URL: string = 'https://www.walliea.ai:4000';
+
 export default BASE_URL;
